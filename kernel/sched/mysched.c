7a2b2a0c114687d8cbe20675e159f2c977bd5a12
HyeonggeunYun <yhg8423@gmail.com>
2019-11-03 19:11:48 +0900

    Update pick_next_task in kernel/sched/mysched.c

--- kernel/sched/mysched.c
+++ kernel/sched/mysched.c
@@ -67,7 +67,7 @@ pick_next_task_mysched(struct rq *rq, struct task_struct *prev, struct rq_flags 
 
     printk(KERN_INFO "***[MYSCHED] total = [%d]\n", rq->mysched.max_ticket);
     printk(KERN_INFO "***[MYSCHED] winner = [%d]\n", lucky_ticket);
-ㅊ
+
     /*
     struct list_head *q;
     list_for_each(q, &mysched_rq->queue) {
